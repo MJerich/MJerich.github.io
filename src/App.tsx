f199f012464d56fda663a47675a3f4c879f5a999
MJerich <MatthewJerich@gmail.com>
2023-10-21 00:44:08 -0500

    changed file structure and logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "./assets/images/logo.png";
+import "./assets/css/App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>cool dude site, coming soon.</p>
       </header>
     </div>
   );
